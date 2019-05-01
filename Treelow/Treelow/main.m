d3fc6f0c39d23ddee5c323fea9c90e09cb24d51c
LuizRamos <luizramospe@hotmail.com>
2019-05-01 17:55:44 -0400

    Created XCode Project

---
+++ Treelow/Treelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Treelow
+//
+//  Created by Luiz on 5/1/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
